07a66a546fcc5a0c7684b265013b543b0c0067df
Léo Fernandes <leandroleobh@gmail.com>
2024-02-08 07:47:27 -0300

    feat: Implementing error handler

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,20 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_erros(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        # Enviar para um log
+        # Enviar para um e-mail de notificação
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "details": error.message
+                }]
+            }
+        )
+    
     return HttpResponse(
         status_code=500,
         body={
